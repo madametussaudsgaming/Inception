684fe7fa6a8f04dd3520311355030c353ddc9aec
rpadasia <rpadasia@vbox.myguest.virtualbox.org>
2026-06-04 17:19:25 +0800

    attempting to fix env incorrections

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,15 +4,25 @@
 sed -i 's/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/g' \
     /etc/mysql/mariadb.conf.d/50-server.cnf
 
+# create socket directory and give mysql user ownership
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
 # starts MariaDB temporarily (to run SQL commands)
-service mysql start
+#service mysql start
 #NOTES
 #despite saying mysql, we are actually running mariadb
 #this way anything written for mysql would work identically with mariadb
+#service don't work the same way in containers, so this is not correct
 
-echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > toberead.sql
-echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> toberead.sql
-echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> toberead.sql
+#start mariadb without service system
+mysqld_safe --skip-networking &
+sleep 3
+
+
+echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > toberead.sql
+echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> toberead.sql
+echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> toberead.sql
 #set password
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db1_root_pwd' ;" >> toberead.sql
 #reload permission tables so changes take effect asap
@@ -28,5 +38,7 @@ kill $(cat /var/run/mysqld/mysqld.pid)
 #if it dies, the container gets killed. daemons start and then go into the background
 #to the terminal and Docker, it has finished executing and thus the container no longer serves a purpose.
 
-# runs MariaDB directly in the foreground, no daemonizing
-mysqld
+# runs MariaDB directly in the foreground, no daemonizing, AS THE USER NOT ROOT
+mysqld --user=mysql
+#NOTES
+#mariadb refuses to run as root typically, docker runs everything as root
